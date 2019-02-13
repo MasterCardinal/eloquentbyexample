74566ea4757ced6437c6f50a931b19fc260157eb
MasterCardinal <74437+MasterCardinal@users.noreply.github.com>
2019-02-13 19:25:59 +0100

    Initial Commit
    From Dräsdn

---
+++ database/seeds/DogsTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DogsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Dog::truncate();
+
+        (new Faker\Generator)->seed(123);
+
+        factory(App\Dog::class, 50)->create();
+    }
+}
